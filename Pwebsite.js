4e9fe5189383cbfbf4db12bd09f1f207397d5e76
myname21-meet <myname21@meet.mit.edu>
2019-07-25 11:43:30 -0400

    Submitting final project

---
+++ Pwebsite.js
@@ -0,0 +1,8 @@
+$("#Button").on("click", function(){
+	$("svg").toggleClass("Apearing");
+});
+$("#Button2").on("click", function(){
+	$("svg").toggleClass("Apearingnot");
+});
+
+$("circle").css("", "yellow")
